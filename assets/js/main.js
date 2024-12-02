d3491835aaad9bd08d41df3ec323b906cb005305
YogananthJ <mjyogananth@gmail.com>
2024-12-02 18:45:01 +0530

    theme code added

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+// DARK/LIGHT THEME
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun";
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+// Obtain the current theme
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+
+// Apply the default or previously selected theme
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
+  themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](iconTheme);
+} else {
+  // Default to dark theme if no selection is stored
+  document.body.classList.add(darkTheme);
+  themeButton.classList.add(iconTheme);
+}
+
+// Activate/Deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark icon/theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // Save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
+
